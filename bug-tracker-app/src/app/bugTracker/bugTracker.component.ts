da84974faef2b5ab29c806f8f296bd1e59560ab0
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-16 15:50:05 +0530

    Bug as an object

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,9 +1,25 @@
 import { Component } from "@angular/core";
+import { Bug } from "./models/Bug";
 
 @Component({
     selector : 'app-bug-tracker',
     templateUrl : './bugTracker.component.html'
 })
 export class BugTrackerComponent{
-    
+    bugs : Bug[] = [];
+    private prevBugId: number = 0;
+
+    onAddNewClick(newBugName : string){
+        const newBug  : Bug = {
+            id : ++this.prevBugId,
+            name : newBugName,
+            isClosed : false,
+            createdAt : new Date()
+        }
+        this.bugs.push(newBug);
+    }
+
+    onRemoveClick(bugToRemove : Bug){
+        this.bugs = this.bugs.filter(bug => bug.id !== bugToRemove.id);
+    }
 }
