7ffc37e5cfdd5fc953b3d7312ee28b68ba96cd49
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-28 11:32:35 +0530

    use case : 10. Improvise 'createdAt' for the bugs

--- bug-tracker-app/src/app/utils/utils.module.ts
+++ bug-tracker-app/src/app/utils/utils.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { SortPipe } from "./pipes/sort.pipe";
 import { TrimTextPipe } from "./pipes/trimText.pipe";
-
+import { ElapsedPipe } from "./pipes/elapsed.pipe";
 import { SortService } from "./services/sort.service";
 
 @NgModule({
     declarations : [
         SortPipe,
-        TrimTextPipe
+        TrimTextPipe,
+        ElapsedPipe
     ],
     providers : [
         SortService
@@ -18,6 +19,7 @@ import { SortService } from "./services/sort.service";
     exports : [
         SortPipe
         , TrimTextPipe
+        , ElapsedPipe
     ]
 })
 export class UtilsModule{
